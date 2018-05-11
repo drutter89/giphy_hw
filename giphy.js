5f2f78b56362c8a89a7f1de485c03e582d644823
Dylan Rutter <dylanrutter@Dylans-MBP.attlocal.net>
2018-05-11 15:07:04 -0500

    got it

---
+++ giphy.js
@@ -0,0 +1,68 @@
+$(function () { // body of the function "block" between two curly braces. All jquery inside body of function so it runs when document is ready.
+  $("button").on("click", function () {
+    var theme = $(this).attr("theme");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      theme + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+      .then(function (response) {
+        var results = response.data;
+        console.log(results)
+        for (let gif of results) {
+          let gifDiv = `
+            <div class='item'>
+              <img 
+                src=${gif.images.fixed_height_still.url}
+                data-still=${gif.images.fixed_height_still.url}
+               data-animate=${gif.images.fixed_height.url}
+               data-state='still'
+              >
+              <p>Rating ${gif.rating}</p>
+            </div>
+          `
+          $("#gifs-appear-here").prepend(gifDiv)
+        }
+      });
+  });
+
+  $("#gifs-appear-here").on("click", "img", function (event) {
+    var state = $(this).attr('data-state');
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  })
+
+})
+// if you try to dynamically create an element on the page through j query you wont be able to tie an event handler to that element you have to use event delegation/event bubbling/event propegation
+/*
+
+
+     for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div class='item'>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var themeImage = $("<img>");
+          themeImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(themeImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+
+
+          var state = $(this).attr("data-state");
+          // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+          // Then, set the image's data-state to animate
+          // Else set src to the data-still value
+     }     
+*/
